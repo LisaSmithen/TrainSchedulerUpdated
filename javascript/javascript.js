5ff257e522e0437a17db92ea219d6a2cece15140
Lisa Smithen <smithenlisa@gmail.com>
2019-09-23 15:00:39 -0500

    Javascript coding-removed to fix some of the errors that were showing up in the console. Styling created through adding image and additional css coding

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+
 
     //Attach Firebase vars config thing
 
-var firebaseConfig = {
+var config = {
     apiKey: "AIzaSyCwGmczVpOv_WOMM13odPxw5VSl92KnR2c",
     authDomain: "trainjourney-87e60.firebaseapp.com",
     databaseURL: "https://trainjourney-87e60.firebaseio.com",
@@ -14,7 +14,7 @@ var firebaseConfig = {
 
 
 //Initialize Firebase app
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 
 //Firebase var
 var database = firebase.database();
@@ -34,11 +34,39 @@ $("#add-train").on("click", function() {
     frequency = $("#frequency").val().trim();
 
 
-
-
-
-
-
-
-
+    database.ref().push({
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+    $("form")[0].reset();
 });
+
+//Append the destinantion, frequency, next train ....
+database.ref().on("child_added", function(childSnapshot) {
+    var nextArr;
+    var minAway;
+    var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
+    var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+    var remainder = diffTime % childSnapshot.val().frequency;
+    var minAway = childSnapshot.val().frequency - remainder;
+    var nextTrain = moment().add(minAway, "minutes");
+    nextTrain = moment(nextTrain).format("hh:mm");
+
+//Snapshot
+    $("#add-row").append("<tr><td>" + childSnapshot.val().name +
+            "</td><td>" + childSnapshot.val().destination +
+            "</td><td>" + childSnapshot.val().frequency +
+            "</td><td>" + nextTrain + 
+            "</td><td>" + minAway + "</td></tr>");
+
+            database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+              
+                $("#name-display").html(snapshot.val().name);
+
+
+            });
+
+        });
